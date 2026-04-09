d3cfd828520159f7f8eb3af7ede2ecc64242e616
ramenmac <kevinnguyen4200@gmail.com>
2026-04-09 13:15:52 -0500

    1st Step variable initialization and outputing

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+public class VariableApp {
+
+    public static void main(String[] args) {
+
+        String favoriteColor = "yellow";
+        System.out.println("My favorite color is " + favoriteColor);
+
+        short yearStartedProgram = 2026;
+        System.out.println("I started Year Up in " + yearStartedProgram);
+
+        char middleInitial = 'L';
+        System.out.println("My middle initial is " + middleInitial);
+
+        boolean hasPets = true;
+        System.out.println("Do I have pet? " + hasPets);
+
+        String niceGreeting = "I hope you're having a great day";
+        System.out.println(niceGreeting);
+
+    }
+}
